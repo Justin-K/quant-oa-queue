e6eae18b5be5aaddf0e5a33a466b5b36d7baf8ee
Justin K <justinj.kirkland@gmail.com>
2022-09-23 14:27:31 -0500

    Updated files.
    The logic for array expansion is now contained in a private method, expandArray().
    Wrote the push() method to add an element to the queue and resize it if it's full.
    Both of these methods must be tested because it's quite possible I'm not understanding
    something correctly, or some other error is occurring.

--- queue_java/src/quant/QuantQueue.java
+++ queue_java/src/quant/QuantQueue.java
@@ -9,6 +9,7 @@ public class QuantQueue<Type>{
     private int tail;
     private final Class<Type> type;
 
+    @SuppressWarnings("unchecked")
     public QuantQueue(Class<Type> t){
         this.type = t;
         // This is sketch
@@ -16,22 +17,26 @@ public class QuantQueue<Type>{
         head = 0;
         tail = DEFAULT_SIZE - 1;
     }
+    @SuppressWarnings("unchecked")
+    private void expandArray(int new_size){
+        Type[] new_array = (Type[]) Array.newInstance(type, new_size);
+        // Remove literal indices and test
+        System.arraycopy(array, 0, new_array, 1, new_array.length);
+        array = new_array;
+    }
+
     public void push(final Type entry){
         if ((tail == DEFAULT_SIZE) && (head == 0)) {
-            Type[] new_array = (Type[]) Array.newInstance(this.type, DEFAULT_SIZE * 2);
-            // Remove literal indices and test
-            System.arraycopy(this.array, 0, new_array, 1, new_array.length);
-            array = new_array;
+            expandArray(DEFAULT_SIZE);
         }
-        else {
-
-        }
+        tail++;
+        array[tail] = entry;
     }
-    public Type front(){}
-    public Type back(){}
-    public Boolean isEmpty(){}
-    public int size(){}
-    public void emplace(final Type entry){}
-    public Type pop(){}
-    public void swap(final int index_1, final int index_2){}
+//    public Type front(){}
+//    public Type back(){}
+//    public Boolean isEmpty(){}
+//    public int size(){}
+//    public void emplace(final Type entry){}
+//    public Type pop(){}
+//    public void swap(final int index_1, final int index_2){}
 }
