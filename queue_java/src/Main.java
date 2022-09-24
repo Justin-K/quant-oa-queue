83e7e71023a11a94b5ba8073f8add663720ad357
Justin K <justinj.kirkland@gmail.com>
2022-09-23 23:45:32 -0500

    Wrote examples of QuantQueue usage.

--- queue_java/src/Main.java
+++ queue_java/src/Main.java
@@ -1,31 +1,17 @@
 import quant.QuantQueue;
 
 public class Main {
-    public static int[] fillArray(int[] array){
-        int[] new_array = new int[array.length];
-        for (int i = 0; i < array.length; i++){
-            new_array[i] = i + 1;
-        }
-        return new_array;
-    }
 
-
-    public static void main(String[] args){
-        QuantQueue<Integer> x = new QuantQueue<>(Integer.class);
-
-        System.out.println(x.isEmpty());
-        //x.push(1);
-        //System.out.println(x.tail);
-        //x.printArray();
-//        int[] src = new int[5];
-//        int[] dest = new int[10];
-//        src = fillArray(src);
-//
-//        System.arraycopy(src, 0, dest, 0, src.length);
-//
-//
-//        printArray(dest);
-
-
+    public static void main(String[] args) {
+        QuantQueue<Integer> quantQueue = new QuantQueue<>(Integer.class);
+        System.out.println(quantQueue.isEmpty()); // This true
+        quantQueue.push(1);
+        quantQueue.push(2);
+        quantQueue.push(3);
+        System.out.println(quantQueue.pop()); // This prints 1
+        System.out.println(quantQueue.front()); // This prints 2
+        System.out.println(quantQueue.back()); // This prints 3
+        System.out.println(quantQueue.size()); // This prints 2
+        System.out.println(quantQueue.isEmpty()); // This prints false
     }
 }
